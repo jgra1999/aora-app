d1059c04f37cbbd76d651aac82e45c9c62ca2e19
Jose Rojas <jgra11.2010@gmail.com>
2024-07-04 20:36:44 -0400

    fonts added to the project and to the layout

--- app/index.tsx
+++ app/index.tsx
@@ -2,14 +2,8 @@ import { Text, View } from 'react-native'
 
 export default function Index() {
 	return (
-		<View
-			style={{
-				flex: 1,
-				justifyContent: 'center',
-				alignItems: 'center'
-			}}
-		>
-			<Text>My App</Text>
+		<View className='flex-1 items-center justify-center bg-red-500'>
+			<Text className='font-psemibold'>My App</Text>
 		</View>
 	)
 }
