c29f34a4570e9a98b057bedd51ead8572424c50a
Jose Rojas <jgra11.2010@gmail.com>
2024-07-08 14:11:26 -0400

    Button component created

---
+++ components/ui/CustomButton.tsx
@@ -0,0 +1,32 @@
+import { View, Text, TouchableOpacity } from 'react-native'
+
+interface Props {
+	title: string
+	containerStyles?: string
+	textStyles?: string
+	isLoading?: boolean
+	event: () => void
+}
+
+export function CustomButton({
+	title,
+	containerStyles,
+	event,
+	isLoading,
+	textStyles
+}: Props) {
+	return (
+		<TouchableOpacity
+			className={`bg-secondary rounded-xl min-h-[62px] justify-center items-center ${containerStyles} ${
+				isLoading ? 'opacity-50' : ''
+			}`}
+			onPress={event}
+			activeOpacity={0.7}
+			disabled={isLoading}
+		>
+			<Text className={`text-primary text-lg font-psemibold ${textStyles}`}>
+				{title}
+			</Text>
+		</TouchableOpacity>
+	)
+}
